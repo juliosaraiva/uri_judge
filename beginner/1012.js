e1d2d1b4ae73c13e6623b1cab7f7d36f96083b2c
Julio Saraiva <contato@juliosaraiva.com.br>
2020-06-14 23:38:51 -0300

    chore: solved problem 1012

--- beginner/1012.js
+++ beginner/1012.js
@@ -13,19 +13,45 @@ Output
 The output file must contain 5 lines of data. Each line corresponds to one of the areas described above, always with a corresponding message (in Portuguese) and one space between the two points and the value. The value calculated must be presented with 3 digits after the decimal point.
 
 ---
+var [A, B, C] = lines.shift().split(' ');
+const PI = 3.14159;
+var areaTriangle = (parseFloat(A) * parseFloat(C)) / 2;
+var areaCircle = PI * Math.pow(parseFloat(C), 2);
+var areaTrapezium = ((parseFloat(A) + parseFloat(B)) * parseFloat(C)) / 2;
+var areaSquare = Math.pow(parseFloat(B), 2);
+var areaRectangle = parseFloat(A) * parseFloat(B);
+console.log("TRIANGULO: " + areaTriangle.toFixed(3));
+console.log("CIRCULO: " + areaCircle.toFixed(3));
+console.log("TRAPEZIO: " + areaTrapezium.toFixed(3));
+console.log("QUADRADO: " + areaSquare.toFixed(3))
+console.log("RETANGULO: " + areaRectangle.toFixed(3))
 ---
 
->>> response(3.0, 4.0, 5.2)
+> response(3.0, 4.0, 5.2)
 TRIANGULO: 7.800
 CIRCULO: 84.949
 TRAPEZIO: 18.200
 QUADRADO: 16.000
 RETANGULO: 12.000
 
->>> reponse(12.7, 10.4, 15.2)
+> reponse(12.7, 10.4, 15.2)
 TRIANGULO: 96.520
 CIRCULO: 725.833
 TRAPEZIO´: 175.560
 QUADRADO: 108.160
 RETANGULO: 132.080
 */
+
+function response(A, B, C) {
+  const PI = 3.14159;
+  var areaTriangle = (parseFloat(A) * parseFloat(C)) / 2;
+  var areaCircle = PI * Math.pow(parseFloat(C), 2);
+  var areaTrapezium = ((parseFloat(A) + parseFloat(B)) * parseFloat(C)) / 2;
+  var areaSquare = Math.pow(parseFloat(B), 2);
+  var areaRectangle = parseFloat(A) * parseFloat(B);
+  console.log("TRIANGULO: " + areaTriangle.toFixed(3));
+  console.log("CIRCULO: " + areaCircle.toFixed(3));
+  console.log("TRAPEZIO: " + areaTrapezium.toFixed(3));
+  console.log("QUADRADO: " + areaSquare.toFixed(3))
+  console.log("RETANGULO: " + areaRectangle.toFixed(3))
+}
